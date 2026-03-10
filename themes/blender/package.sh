12ffc967149bffd940eab63777ee410f7ee77e4b
Catzs <ari.goodland@pm.me>
2026-03-09 20:33:52 -0600

    Fixed packaging script's directory structuring

--- themes/blender/package.sh
+++ themes/blender/package.sh
@@ -6,22 +6,23 @@ FILES=("Tokyo_Night.xml" "blender_manifest.toml")
 FILENAME="tokyonight_blender.zip"
 MISSING=()
 
-for f in "${!FILES[@]}"; do
-    file="$DIR/${FILES[f]}"
+cd "$DIR"
+for file in "${FILES[@]}"; do
     if ! [[ -e "$file" ]]; then
-        MISSING+=(${FILES[f]})
+        MISSING+=($file)
     fi
-    FILES[f]=$file
 done
 
 if [[ ${#MISSING[@]} -gt 0 ]]; then
     echo "The following files cannot be found:"
     for file in "${MISSING[@]}"; do
-        printf "\t$file\n"
+        printf "\t- $file\n"
     done
     exit 1
 fi
 
-zip -q "$DIR/$FILENAME" "${FILES[@]}"
-echo "Successfully packaged '$FILENAME'."
-exit 0
+if zip -q9r "$FILENAME" . -i "${FILES[@]}"; then
+    echo "Successfully packaged '$FILENAME'."
+    exit 0
+fi 
+exit 1
